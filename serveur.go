2dbec16f9a5db4e7a6182a07eff21efdd97001a2
julesoutin <jules.outin@ynov.com>
2022-02-28 13:43:07 +0100

    update

--- serveur.go
+++ serveur.go
@@ -13,12 +13,58 @@ import (
 
 type Page struct {
 	Name         string
+	Image        string
 	Members      []string
 	CreationDate string
 	FirstAlbum   string
 	Locations    string
 	ConcertDates string
 	Relations    string
+}
+type Page2 struct {
+	Name  []string
+	Image []string
+}
+
+type Todo struct {
+	Id           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Location     string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
+
+func HomePage(adress string) interface{} {
+	fmt.Println("1. Performing Http Get...")
+	var id = 1
+	var test = ""
+	var name []string
+	var image []string
+	var tab Todo
+	for id != 0 {
+		test = "/" + strconv.Itoa(id)
+		resp, err := http.Get(adress + test)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer resp.Body.Close()
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		json.Unmarshal(bodyBytes, &tab)
+		name = append(name, tab.Name)
+		image = append(image, tab.Image)
+		id = tab.Id
+		if id == 0 {
+			break
+		}
+		id++
+	}
+	data := Page2{name, image}
+	fmt.Println(data)
+	return data
 }
 
 func get(adress string, nbArtist int) (interface{}, int) {
@@ -36,6 +82,7 @@ func get(adress string, nbArtist int) (interface{}, int) {
 	// Convert response body to tab interface
 	json.Unmarshal(bodyBytes, &tab)
 	var name string
+	var image string
 	var members []string
 	var creationDate string
 	var firstAlbum string
@@ -64,22 +111,30 @@ func get(adress string, nbArtist int) (interface{}, int) {
 		if key == "relations" {
 			relations = fmt.Sprint(value)
 		}
+		if key == "image" {
+			image = fmt.Sprint(value)
+		}
 	}
-	data := Page{name, members, creationDate, firstAlbum, location, concertDates, relations}
+	data := Page{name, image, members, creationDate, firstAlbum, location, concertDates, relations}
 	return data, valreturn
 }
 
 func main() {
+	var fs = http.FileServer(http.Dir("assets/"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	lien := "https://groupietrackers.herokuapp.com/api"
-	fileServer := http.FileServer(http.Dir("assets")) //Envoie des fichiers aux serveurs (CSS, sons, images)
-	http.Handle("/assets/", http.StripPrefix("/assets/", fileServer))
+	// fileServer := http.FileServer(http.Dir("assets/")) //Envoie des fichiers aux serveurs (CSS, sons, images)
+	// http.Handle("/assets/", http.StripPrefix("/assets/", fileServer))
 	// affiche l'html
-	tmpl, err := template.ParseFiles("./templates/navPage.gohtml")
+	tmpl, err := template.ParseFiles("./assets/layout.html", "./assets/header.html", "./assets/footer.html")
 	if err != nil {
 	}
-	nb := 4
+	nb := 0
 	http.HandleFunc("/Groupie-tracker", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
+			tmpl, err = template.ParseFiles("./assets/layout.html")
+			if err != nil {
+			}
 			nb, _ = strconv.Atoi(r.FormValue("nombre"))
 		}
 		data, codeError := get(lien+"/artists", nb)
@@ -91,12 +146,22 @@ func main() {
 
 	http.HandleFunc("/Groupie-tracker/artist", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
-			tmpl, err = template.ParseFiles("./templates/index.gohtml")
+			tmpl, err = template.ParseFiles("./assets/index.gohtml")
 			nb, _ = strconv.Atoi(r.FormValue("nombre"))
 		}
 		data, codeError := get(lien+"/artists", nb)
 		if codeError == 500 {
+		}
+		tmpl.ExecuteTemplate(w, "index", data)
+	})
 
+	http.HandleFunc("/Groupie-tracker/artist=", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			tmpl, err = template.ParseFiles("./assets/artistes.gohtml")
+			nb, _ = strconv.Atoi(r.FormValue("nombre"))
+		}
+		data, codeError := get(lien+"/artists", nb)
+		if codeError == 500 {
 		}
 		tmpl.ExecuteTemplate(w, "index", data)
 	})
