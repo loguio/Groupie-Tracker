4b1382ac047cb0138b1996586b7aab2660aa3f4e
JulesOutin <jules.outin@ynov.com>
2022-02-28 21:04:13 +0100

    Mise en place multipage

--- serveur.go
+++ serveur.go
@@ -11,161 +11,207 @@ import (
 	"strings"
 )
 
-type Page struct {
-	Name         string
-	Image        string
-	Members      []string
-	CreationDate string
-	FirstAlbum   string
-	Locations    string
-	ConcertDates string
-	Relations    string
-}
-type Page2 struct {
-	Name  []string
-	Image []string
-}
+//on Importe toute les bibliothèques que l'on a besoin
 
-type Todo struct {
-	Id           int      `json:"id"`
-	Image        string   `json:"image"`
-	Name         string   `json:"name"`
-	Members      []string `json:"members"`
-	CreationDate int      `json:"creationDate"`
-	FirstAlbum   string   `json:"firstAlbum"`
-	Location     string   `json:"locations"`
-	ConcertDates string   `json:"concertDates"`
-	Relations    string   `json:"relations"`
-}
+type ArtistAPI struct {
+	Id                  int      `json:"id"`
+	Image               string   `json:"image"`
+	Name                string   `json:"name"`
+	Members             []string `json:"members"`
+	CreationDate        int      `json:"creationDate"`
+	FirstAlbum          string   `json:"firstAlbum"`
+	AddressLocation     string   `json:"locations"`
+	ConcertDatesaddress string   `json:"concertDates"`
+	RelationsAdress     string   `json:"relations"`
+	Location            []string
+	ConcertDates        []string
+	Relations           map[string][]string
+	RelationDate        [][]string
+	DateLocation        []DateLocation
+} // on créer une structure qui contient toutes les données pouvant etre utile a notre site cela va nous permettre d'afficher chaque groupe avec leur données respectives
 
-func HomePage(adress string) interface{} {
+type DateLocation struct {
+	Location string
+	Dates    []string
+} //Cette structure nous permet d'afficher les lieu ainsi que les dates des spectacles
+
+type Location struct {
+	Id       int      `json:"id"`
+	Location []string `json:"locations"`
+	Dates    string   `json:"dates"`
+} // cette strcture nous permet de recuperer les donnée du lien API Location
+
+type Dates struct {
+	Id    int      `json:"id"`
+	Dates []string `json:"dates"`
+} // cette strcture nous permet de recuperer les donnée du lien API Dates
+
+type Relation struct {
+	Id             int                 `json:"id"`
+	DatesLocations map[string][]string `json:"datesLocations"`
+} // cette strcture nous permet de recuperer les donnée du lien API Relation
+
+func ArtistPage(adress string, Page int) interface{} { //Cette fonction se lance lorsque l'utilisateur est sur la page des artistes
 	fmt.Println("1. Performing Http Get...")
-	var id = 1
-	var test = ""
-	var name []string
-	var image []string
-	var tab Todo
-	for id != 0 {
-		test = "/" + strconv.Itoa(id)
-		resp, err := http.Get(adress + test)
+	var idArtist = (Page-1)*12 + 1 // on prend le première identifiant de l'artiste que l'utilisateur veut afficher
+	var url = ""
+	var artists []ArtistAPI // nos artistes seront stockés dans cette variables
+	var oneArtist ArtistAPI //on stock les données de un artiste danc cette variable
+	var nettoyage []DateLocation
+	fmt.Println("1. Performing Http Get...")
+	fmt.Println("2. Le serveur est lancé sur le port 3000")
+	for idArtist != Page*12+1 { // on repete cette action jusqu'a ce qu'on ait recupéré les données de 12 artistes
+		var relationdate [][]string
+		url = "/" + strconv.Itoa(idArtist)  // On recupère un URL equivalent a afficher les données avec l'ID d'un artiste
+		resp, err := http.Get(adress + url) // on recupère les données qui sont stockés dans resp
 		if err != nil {
-			log.Fatalln(err)
+			log.Fatalln(err) // si il y a une erreur donc erreur
 		}
 		defer resp.Body.Close()
 		bodyBytes, _ := ioutil.ReadAll(resp.Body)
-		json.Unmarshal(bodyBytes, &tab)
-		name = append(name, tab.Name)
-		image = append(image, tab.Image)
-		id = tab.Id
-		if id == 0 {
+		json.Unmarshal(bodyBytes, &oneArtist) // on implémente les données contenus dans bodyBytes dans oneArtist cela va nous permettre de recupérer les données
+		idArtist = oneArtist.Id
+		if idArtist == 0 {
 			break
+		} // si l'id est égal a 0 c'est que l'on a atteint la fin des artistes et que il n'y en a pas plus a afficher donc on break pour sortir de la boucle
+		oneArtist.FirstAlbum = gooddate(oneArtist.FirstAlbum)               //on passe la donnée FirstAlbum dans la fonction gooddate pour avoir une date plus explicite
+		oneArtist.Location = location(oneArtist.AddressLocation)            // on Récupere les données qui nous interesse grace a la fonction Location car AddressLocation est un lien API
+		oneArtist.ConcertDates = concertdate(oneArtist.ConcertDatesaddress) // ConcertDatesaddress est aussi un lien API donc on tri les données pur avoir celle qui nous intéresse
+		oneArtist.Relations = relation(oneArtist.RelationsAdress)           // on fait la meme chose pour RelationsAdress
+		for i := 0; i < len(oneArtist.Location); i++ {
+			for k := 0; k < len(oneArtist.Relations[oneArtist.Location[i]]); k++ {
+				oneArtist.Relations[oneArtist.Location[i]][k] = gooddate(oneArtist.Relations[oneArtist.Location[i]][k]) //on change aussi la date des concert our avoir une date plus joli
+			}
+			relationdate = append(relationdate, oneArtist.Relations[oneArtist.Location[i]]) // on rajoute les valeurs des dates dans l'index de la villes correspondante
 		}
-		id++
+		oneArtist.RelationDate = relationdate // on stock les valeurs des dates dans
+		oneArtist.DateLocation = nettoyage    // on vide notre liste
+		for i := 0; i < len(oneArtist.Location); i++ {
+			var tempo DateLocation
+			tempo.Location = bonLieu(oneArtist.Location[i])
+			tempo.Dates = oneArtist.RelationDate[i]
+			oneArtist.DateLocation = append(oneArtist.DateLocation, tempo) // on ajoute les valeurs utile dans DateLocation
+		}
+		artists = append(artists, oneArtist) // on ajoute un artiste par un dans artists
+		idArtist++                           // on incremente idArtists pour avoir l'artiste suivant avec l'URL
+		fmt.Println(oneArtist)
 	}
-	data := Page2{name, image}
-	fmt.Println(data)
-	return data
+	return artists // on renvois notre liste avec 12 artistes et les données
 }
 
-func get(adress string, nbArtist int) (interface{}, int) {
-	valreturn := 0
-	fmt.Println("1. Performing Http Get...")
-	fmt.Println("2. Le serveur est lancé sur le port 3000")
+func bonLieu(date string) string {
+	// cette fonctions permet d'enlever les caractères qui ne servent a rien
+	date = strings.Replace(date, "-", " ", -1)
+	date = strings.Replace(date, "_", " ", -1)
+	return date
+}
+
+func relation(adress string) map[string][]string {
+	//relation nous permet de prendre les valeurs que l'on a vraiment beosin dans L'API Relation
+	var relation Relation
 	resp, err := http.Get(adress)
 	if err != nil {
 		log.Fatalln(err)
-		valreturn = 500
 	}
-	defer resp.Body.Close()
 	bodyBytes, _ := ioutil.ReadAll(resp.Body)
-	var tab interface{}
-	// Convert response body to tab interface
-	json.Unmarshal(bodyBytes, &tab)
-	var name string
-	var image string
-	var members []string
-	var creationDate string
-	var firstAlbum string
-	var location string
-	var concertDates string
-	var relations string
-	for key, value := range tab.([]interface{})[nbArtist].(map[string]interface{}) {
-		if key == "name" {
-			name = fmt.Sprint(value)
-		}
-		if key == "members" {
-			members = strings.Split(fmt.Sprint(value), " ")
-		}
-		if key == "creationDate" {
-			creationDate = fmt.Sprint(value)
-		}
-		if key == "firstAlbum" {
-			firstAlbum = fmt.Sprint(value)
-		}
-		if key == "locations" {
-			location = fmt.Sprint(value)
-		}
-		if key == "concertDates" {
-			concertDates = fmt.Sprint(value)
-		}
-		if key == "relations" {
-			relations = fmt.Sprint(value)
-		}
-		if key == "image" {
-			image = fmt.Sprint(value)
-		}
+	json.Unmarshal(bodyBytes, &relation)
+	return relation.DatesLocations
+}
+
+func concertdate(adress string) []string {
+	// Concertdate nous permet de recuperer les location et les dates dans l'API
+	var dates Dates
+	resp, err := http.Get(adress)
+	if err != nil {
+		log.Fatalln(err)
 	}
-	data := Page{name, image, members, creationDate, firstAlbum, location, concertDates, relations}
-	return data, valreturn
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+	json.Unmarshal(bodyBytes, &dates)
+	for i := 0; i < len(dates.Dates); i++ {
+		dates.Dates[i] = gooddate(dates.Dates[i])
+	}
+	return dates.Dates
+}
+
+func gooddate(mois string) string {
+	// cette fonction nous permet de changer les mois de l'année pour les avoir en texte et on enleve les caractères qui servent a rien
+	tempo := strings.Split(mois, "-")
+	if tempo[1] == "01" {
+		mois = strings.Replace(tempo[1], "01", "Janvier", -1)
+	} else if tempo[1] == "02" {
+		mois = strings.Replace(tempo[1], "02", "Fevrier", -1)
+	} else if tempo[1] == "03" {
+		mois = strings.Replace(tempo[1], "03", "Mars", -1)
+	} else if tempo[1] == "04" {
+		mois = strings.Replace(tempo[1], "04", "Avril", -1)
+	} else if tempo[1] == "05" {
+		mois = strings.Replace(tempo[1], "05", "Mai", -1)
+	} else if tempo[1] == "06" {
+		mois = strings.Replace(tempo[1], "06", "Juin", -1)
+	} else if tempo[1] == "07" {
+		mois = strings.Replace(tempo[1], "07", "Juillet", -1)
+	} else if tempo[1] == "08" {
+		mois = strings.Replace(tempo[1], "08", "Aout", -1)
+	} else if tempo[1] == "09" {
+		mois = strings.Replace(tempo[1], "09", "Septembre", -1)
+	} else if tempo[1] == "10" {
+		mois = strings.Replace(tempo[1], "10", "Octobre", -1)
+	} else if tempo[1] == "11" {
+		mois = strings.Replace(tempo[1], "11", "Novembre", -1)
+	} else if tempo[1] == "12" {
+		mois = strings.Replace(tempo[1], "12", "Decembre", -1)
+	}
+	tempo[1] = mois
+	mois = strings.Join(tempo, " ")
+	mois = strings.Replace(mois, "*", "", -1)
+	return mois
+}
+
+func location(adress string) []string {
+	// on recupere les information qui nous interresse dans locations
+	var locations Location
+	resp, err := http.Get(adress)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+	json.Unmarshal(bodyBytes, &locations)
+	return locations.Location
 }
 
 func main() {
-	var fs = http.FileServer(http.Dir("assets/"))
-	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	lien := "https://groupietrackers.herokuapp.com/api"
-	// fileServer := http.FileServer(http.Dir("assets/")) //Envoie des fichiers aux serveurs (CSS, sons, images)
-	// http.Handle("/assets/", http.StripPrefix("/assets/", fileServer))
+	fileServer := http.FileServer(http.Dir("static/")) //Envoie des fichiers aux serveurs (CSS, sons, images)
+	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
 	// affiche l'html
-	tmpl, err := template.ParseFiles("./assets/layout.html", "./assets/header.html", "./assets/footer.html")
+	tmpl, err := template.ParseFiles("./templates/navPage.html", "./templates/body.html", "./templates/navbar.html", "./templates/footer.html")
 	if err != nil {
 	}
-	nb := 0
+	page := 1
+
 	http.HandleFunc("/Groupie-tracker", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "POST" {
-			tmpl, err = template.ParseFiles("./assets/layout.html")
-			if err != nil {
-			}
-			nb, _ = strconv.Atoi(r.FormValue("nombre"))
-		}
-		data, codeError := get(lien+"/artists", nb)
-		if codeError == 500 {
+		data := ArtistPage(lien+"/artists", page)
+		tmpl.ExecuteTemplate(w, "body", data)
+	})
 
+	http.HandleFunc("/Groupie-tracker/PageSuivante", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			tmpl, err = template.ParseFiles("./templates/navPage.gohtml")
 		}
+		page += 1
+		data := ArtistPage(lien+"/artists", page)
 		tmpl.ExecuteTemplate(w, "index", data)
 	})
 
-	http.HandleFunc("/Groupie-tracker/artist", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/Groupie-tracker/PagePrecedente", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
-			tmpl, err = template.ParseFiles("./assets/index.gohtml")
-			nb, _ = strconv.Atoi(r.FormValue("nombre"))
+			tmpl, err = template.ParseFiles("./assets/navPage.gohtml")
 		}
-		data, codeError := get(lien+"/artists", nb)
-		if codeError == 500 {
-		}
+		page -= 1
+		data := ArtistPage(lien+"/artists", page)
 		tmpl.ExecuteTemplate(w, "index", data)
 	})
 
-	http.HandleFunc("/Groupie-tracker/artist=", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "POST" {
-			tmpl, err = template.ParseFiles("./assets/artistes.gohtml")
-			nb, _ = strconv.Atoi(r.FormValue("nombre"))
-		}
-		data, codeError := get(lien+"/artists", nb)
-		if codeError == 500 {
-		}
-		tmpl.ExecuteTemplate(w, "index", data)
-	})
-
-	fmt.Println("le serveur est en cours d'éxécution a l'adresse http://localhost:3000/Groupie-tracker")
-	http.ListenAndServe("localhost:3000", nil) //lancement du serveur
+	fmt.Println("le serveur est en cours d'éxécution a l'adresse http://localhost:3500/Groupie-tracker")
+	http.ListenAndServe("localhost:3500", nil) //lancement du serveur
 }
