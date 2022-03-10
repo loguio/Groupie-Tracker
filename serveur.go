0b48a689bf0cd4de63e400cf39a998632bd32d31
JulesOutin <jules.outin@ynov.com>
2022-03-10 08:31:26 +0100

    Ajout page cart

--- serveur.go
+++ serveur.go
@@ -18,6 +18,7 @@ func main() {
 	http.HandleFunc("/Groupie-tracker/PagePrecedente", PagePrecedente)
 	http.HandleFunc("/Groupie-tracker/listartist", listartist)
 	http.HandleFunc("/Groupie-tracker/artist", artist)
+	http.HandleFunc("/Groupie-tracker/cart", cart)
 	// http.HandleFunc("/Groupie-trackere", Handle404)
 	fmt.Println("le serveur est en cours d'éxécution a l'adresse http://localhost:3000/Groupie-tracker")
 	http.ListenAndServe("localhost:3000", nil) //lancement du serveur
@@ -85,6 +86,23 @@ func listartist(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 	} //récupération des donnée a envoyer sur la page html
 	tmpl.ExecuteTemplate(w, "listartists", data) //exécution du template
+}
+
+func cart(w http.ResponseWriter, r *http.Request) {
+	lien := "https://groupietrackers.herokuapp.com/api"
+	page := 1
+	data, err := ArtistPage(lien+"/artists", page)                                                                                                                                                                                            //récupération des donnée a envoyer sur la page html
+	tmpl, err := template.ParseFiles("./templates/home.html", "./templates/navbar.html", "./templates/footer.html", "./templates/pageaccueil.html", "./templates/pagelistartists.html", "./templates/pagecart.html", "./templates/cart.html") // utilisation du fichier navPage.gohtml pour le template
+	if err != nil {
+		fmt.Println(err)
+		tmpl, err = template.ParseFiles("./templates/Error500.gohtml") //utilisation du fichier Error500.gohtml pour le template
+		print(err)
+	}
+	// if r.URL.Path != "/" {
+	// 	Handle404(w, r, http.StatusNotFound)
+	// 	return
+	// }
+	tmpl.ExecuteTemplate(w, "cart", data) //exécution du template
 }
 
 func PageSuivante(w http.ResponseWriter, r *http.Request) {
